28afcaa7f5a64a90c728cb379abad8a8733e9984
JP Adams <jp.adams@surfacesteerable.com>
2015-08-12 20:33:48 -0500

    updating settings againg

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -93,7 +93,7 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-# jpx #ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
